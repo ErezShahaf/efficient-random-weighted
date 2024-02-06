fdc2f804f2426267ba7a4ba1de4c907583e39111
ErezShahaf <erezshahaf2@gmail.com>
2024-02-06 19:17:25 +0200

    make statistical tests faster

--- tests/unitTests.test.ts
+++ tests/unitTests.test.ts
@@ -22,49 +22,49 @@ describe('try to create invalid samplers', () => {
     expect(() => createWeightedSampler([{ weight: -0.000001, reward: 10 }])).toThrow()
   })
 })
-// describe('check that the results make sense statistically. It can fail in rare occasuions', () => {
-//   it('should return true', () => {
-//     const mySampler: weightedSampler = createWeightedSampler([
-//       { weight: 1, reward: 0 },
-//       { weight: 1.01, reward: 1 },
-//       { weight: 1.02, reward: 2 },
-//       { weight: 1.03, reward: 3 },
-//       { weight: 1.04, reward: 4 }
-//     ])
-//     const resultArray: number[] = new Array(5).fill(0)
-//     for (let i = 0; i < 1000 * 1000 * 10; i++) {
-//       resultArray[mySampler()]++
-//     }
-//     expect(resultArray[0] < resultArray[1] && resultArray[1] < resultArray[2] && resultArray[2] < resultArray[3] && resultArray[3] < resultArray[4]).toBe(true)
-//   })
-//   it('should return true', () => {
-//     const mySampler: weightedSampler = createWeightedSampler([
-//       { weight: 0.1, reward: 0 },
-//       { weight: 1, reward: 1 },
-//       { weight: 5, reward: 2 },
-//       { weight: 20, reward: 3 },
-//       { weight: 100, reward: 4 }
-//     ])
-//     const resultArray: number[] = new Array(5).fill(0)
-//     for (let i = 0; i < 1000 * 1000 * 2; i++) {
-//       resultArray[mySampler()]++
-//     }
-//     expect(resultArray[0] < resultArray[1] && resultArray[1] < resultArray[2] && resultArray[2] < resultArray[3] && resultArray[3] < resultArray[4]).toBe(true)
-//   })
-//   it('should return true', () => {
-//     const mySampler: weightedSampler = createWeightedSampler([
-//       { weight: 1, reward: 0 },
-//       { weight: 1, reward: 1 }
-//     ])
-//     const resultArray: number[] = new Array(5).fill(0)
-//     for (let i = 0; i < 1000 * 1000 * 2; i++) {
-//       resultArray[mySampler()]++
-//     }
-//     const ratio: number = resultArray[0] / resultArray[1]
-//     console.log(ratio)
-//     expect(ratio < 1.005 && ratio > 0.995).toBe(true)
-//   })
-// })
+describe('check that the results make sense statistically.', () => {
+  it('should return true', () => {
+    const mySampler: weightedSampler = createWeightedSampler([
+      { weight: 1, reward: 0 },
+      { weight: 10, reward: 1 },
+      { weight: 100, reward: 2 },
+      { weight: 1000, reward: 3 },
+      { weight: 10000, reward: 4 }
+    ])
+    const resultArray: number[] = new Array(5).fill(0)
+    for (let i = 0; i < 1000 ; i++) {
+      resultArray[mySampler()]++
+    }
+    expect(resultArray[0] <= resultArray[1] && resultArray[1] <= resultArray[2] && resultArray[2] <= resultArray[3] && resultArray[3] <= resultArray[4]).toBe(true)
+  })
+  it('should return true', () => {
+    const mySampler: weightedSampler = createWeightedSampler([
+      { weight: 0.1, reward: 0 },
+      { weight: 1, reward: 1 },
+      { weight: 10, reward: 2 },
+      { weight: 100, reward: 3 },
+      { weight: 1000, reward: 4 }
+    ])
+    const resultArray: number[] = new Array(5).fill(0)
+    for (let i = 0; i < 1000; i++) {
+      resultArray[mySampler()]++
+    }
+    expect(resultArray[0] <= resultArray[1] && resultArray[1] <= resultArray[2] && resultArray[2] <= resultArray[3] && resultArray[3] <= resultArray[4]).toBe(true)
+  })
+  it('should return true', () => {
+    const mySampler: weightedSampler = createWeightedSampler([
+      { weight: 1, reward: 0 },
+      { weight: 1, reward: 1 }
+    ])
+    const resultArray: number[] = new Array(5).fill(0)
+    for (let i = 0; i < 1000; i++) {
+      resultArray[mySampler()]++
+    }
+    const ratio: number = resultArray[0] / resultArray[1]
+    console.log(ratio)
+    expect(ratio < 1.2 && ratio > 0.8).toBe(true)
+  })
+})
 describe('check reward as object', () => {
   it('should return true', () => {
     const myObject = { value: 10 }
